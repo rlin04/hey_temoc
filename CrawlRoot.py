bf9bd6ce3c9700eab5c81292ef2ef2ff32d82364
Toby Chow <tobychow98@gmail.com>
2019-02-24 06:04:15 -0600

    changes to fit 3 column criteria
    added new column to csv table for Intents
    
    NOTE: need list of intent keywords before successful implementation

--- CrawlRoot.py
+++ CrawlRoot.py
@@ -12,11 +12,10 @@ class CrawlRoot:
         response = http.request('GET', url)
         soup = BeautifulSoup(response.data, features="html.parser")
         # find all the corresponding question and their answer. put in dictionary
-        # question:answer
-        dct = {}
-
-        url_2 = 'https://faq.utdallas.edu'
+        # (intent:question):answer
+        main_list = []
         more_list = []
+        url_2 = 'https://faq.utdallas.edu'
         # find more links
         for link in soup.findAll('li', {'class': 'article article-more'}):
             more_list.append(url_2 + link.find('a')['href'])
@@ -26,13 +25,25 @@ class CrawlRoot:
             for link in soup.findAll('li', {'class': 'article less-important'}):
                 question = link.find('a').text
                 url = (url_2+link.find('a')['href'])
-                dct[question] = url
+                intent = self.find_intent(question)
+                iq_array = [intent, question, url]
+                # create array (intent:question)
+                main_list.append(iq_array)
             # headers = ['question', 'url']
             with open('csv_file.csv', mode='w') as csv_file:
                 writer = csv.writer(csv_file, delimiter = ',', lineterminator='\n')
-                for (question, url) in dct.items():
-                    writer.writerow([question, url])
+                for i in main_list:
+                    writer.writerow([i[0].lower(), i[1].lower(), i[2].lower()])
                     # print(question + " " + url)
+
+    def find_intent(self, question):
+        # initialize list of intents
+        list_intents = ['why']
+        for l in list_intents:
+            if question.__contains__(l):
+                return l
+        return "none"
+
 
     def find_answer(self, question):
 
@@ -40,8 +51,8 @@ class CrawlRoot:
             url = ""
             csv_reader = csv.reader(csv_file, delimiter=",")
             for row in csv_reader:
-                if question == row[0]:
-                    url = row[1]
+                if question == row[1]:
+                    url = row[2]
 
         # find the answer
         http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from CrawlRoot import CrawlRoot
+from pathlib import Path
+from difflib import get_close_matches
+
+def getMatch(patterns, word):
+    print(get_close_matches (word, patterns))
 
 def main():
-    #CrawlRoot().url_to_csv()
-    print(CrawlRoot().find_answer("What is the purpose and scope of UT Dallas's participation in Historically Underutilized Business (HUB) Development?"))
+    #my_file = Path("/csvfile.csv")
+    #if not my_file.exist():
+    CrawlRoot().url_to_csv()
+    print(CrawlRoot().find_answer("What is the purpose and scope of UT Dallas's participation in Historically Underutilized Business (HUB) Development?".lower()))
+
 
 if __name__ == "__main__":
     main()
